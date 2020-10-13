f282378313271e2efbdc5d4a66dd376766ffb8d1
choichunwah <choichunwah@gmail.com>
2020-10-13 21:21:55 +0800

    Add heroku-postbuild

--- knexfile.ts
+++ knexfile.ts
@@ -7,15 +7,12 @@ import path from 'path';
 
 // Const
 const BASE_PATH = path.join('.', 'src', 'server', 'db');
-const DB_URL = `postgres://zwjvfyynkixjbw:dfe9cacadb36434e41288d9e53dd2bf315c9ad9c00925e5e0fcf53ba4e8a70c9@ec2-34-235-62-201.compute-1.amazonaws.com:5432/deff8tb9v2f660`
 const ssl = `?ssl=true`
 
 const knexFile = {
     test: {
         client: 'pg',
         connection: `postgres://${process.env.LOCAL_DB_USER}:${process.env.LOCAL_DB_PASS}@${process.env.LOCAL_DB_HOST}:5432/${process.env.LOCAL_DB_DB}${ssl}`,
-        // connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_DB}`,
-        // connection: `${DB_URL}`,
         migrations: {
             directory: path.join(BASE_PATH, 'migrations')
         },
@@ -26,8 +23,6 @@ const knexFile = {
     development: {
         client: 'pg',
         connection: `postgres://${process.env.LOCAL_DB_USER}:${process.env.LOCAL_DB_PASS}@${process.env.LOCAL_DB_HOST}:5432/${process.env.LOCAL_DB_DB}${ssl}`,
-        // connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_DB}`,
-        // connection: `${DB_URL}`,
         migrations: {
             directory: path.join(BASE_PATH, 'migrations')
         },
@@ -37,8 +32,6 @@ const knexFile = {
     },
     production: {
         client: 'pg',
-        // connection: `postgres://${process.env.LOCAL_DB_USER}:${process.env.LOCAL_DB_PASS}@${process.env.LOCAL_DB_HOST}:5432/${process.env.LOCAL_DB_DB}`,
-        // connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_DB}`,
         connection: `${process.env.DATABASE_URL}${ssl}`,
         migrations: {
             directory: path.join(BASE_PATH, 'migrations')
