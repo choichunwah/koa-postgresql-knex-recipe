86dba2fa331386e296420dbc8d2c17a90e4332f0
choichunwah <choichunwah@gmail.com>
2020-10-13 17:01:15 +0800

    Set Procfile

--- knexfile.js
+++ knexfile.js
@@ -7,13 +7,13 @@ import path from 'path';
 
 // Const
 const BASE_PATH = path.join('.', 'src', 'server', 'db');
-
+const DATABASE_URL = `postgres://zwjvfyynkixjbw:dfe9cacadb36434e41288d9e53dd2bf315c9ad9c00925e5e0fcf53ba4e8a70c9@ec2-34-235-62-201.compute-1.amazonaws.com:5432/deff8tb9v2f660`;
 
 const knexFile = {
     test: {
         client: 'pg',
-        // connection: `postgres://${process.env.DATABASE_URL}`,
-        connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/koa-postgresql-knex-recipe`,
+        connection: `${DATABASE_URL}`,
+        // connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/koa-postgresql-knex-recipe`,
         migrations: {
             directory: path.join(BASE_PATH, 'migrations')
         },
@@ -23,8 +23,8 @@ const knexFile = {
     },
     development: {
         client: 'pg',
-        // connection: `postgres://${process.env.DATABASE_URL}`,
-        connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/koa-postgresql-knex-recipe`,
+        connection: `${DATABASE_URL}`,
+        // connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/koa-postgresql-knex-recipe`,
         migrations: {
             directory: path.join(BASE_PATH, 'migrations')
         },
